125b813d7d7df724969581fac9f9f7624d869268
vinay.ingalahalli <vinay.ingalahalli@revature.com>
2021-03-01 23:02:22 +0530

    java notes and few examples

---
+++ java_examples-Week1/Hello.java
@@ -0,0 +1,16 @@
+public class Hello{
+
+public static void main(String ar[]){
+System.out.println("Hello World.....");
+}
+}
+
+//Single Line Comment
+/*Multi Line Comment
+access specifiers or visibilty modes
+------------------------------------------
+public - visible throughout the app
+protected - visible within the package and any of the child classes which is outside the package
+default - package level scope, only visible within the package
+private - visible only within the class
+*/
---
+++ java_examples-Week1/HeyAgain.java
@@ -0,0 +1,8 @@
+package mypack;
+
+public class HeyAgain{
+
+public static void main(String ar[]){
+System.out.println("Hello World Once Again from Package.....");
+}
+}
